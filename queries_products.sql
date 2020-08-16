3d29dbc802996eca2e09e8ba962c56399bc98aaa
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-08-16 18:20:04 -0500

    Add answer #10

--- queries_products.sql
+++ queries_products.sql
@@ -21,7 +21,7 @@ SELECT * FROM products WHERE can_be_returned = true;
 -- 9. Display only the products that have a price less than 44.00.
 SELECT * FROM products WHERE price < 44.00;
 -- 10. Display only the products that have a price in between 22.50 and 99.99.
-
+SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
 -- 11. There’s a sale going on: Everything is $20 off! Update the database accordingly.
 
 -- 12. Because of the sale, everything that costs less than $25 has sold out. Remove all products whose price meets this criteria.
