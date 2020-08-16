a29287790d9154852ed66163783ae161e0f8ae31
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-08-16 18:07:58 -0500

    Add answer #3

--- queries_products.sql
+++ queries_products.sql
@@ -4,7 +4,7 @@ INSERT INTO products (name, price, can_be_returned) VALUES ("chair", 44.00, fals
 -- 2. Add a product to the table with the name of “stool”, price of 25.99, and can_be_returned of true.
 INSERT INTO products (name, price, can_be_returned) VALUES ('stool', 25.99, 't');
 -- 3. Add a product to the table with the name of “table”, price of 124.00, and can_be_returned of false.
-
+INSERT INTO products (name, price, can_be_returned) VALUES ('table', 124.00, 'f');
 -- 4. Display all of the rows and columns in the table.
 
 -- 5. Display all of the names of the products.
