343b7d0ef54f20039dffbbfe58ebcc7055a1c0f2
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-08-16 18:02:37 -0500

    Add answer #1

--- queries_products.sql
+++ queries_products.sql
@@ -1,6 +1,6 @@
 -- Comments in SQL Start with dash-dash --
 -- 1. Add a product to the table with the name of “chair”, price of 44.00, and can_be_returned of false.
-
+INSERT INTO products (name, price, can_be_returned) VALUES ("chair", 44.00, false);
 -- 2. Add a product to the table with the name of “stool”, price of 25.99, and can_be_returned of true.
 
 -- 3. Add a product to the table with the name of “table”, price of 124.00, and can_be_returned of false.
